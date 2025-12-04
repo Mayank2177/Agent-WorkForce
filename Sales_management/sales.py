6970cce4fa3b7f688957bc6f6e08e8ff3bee278c
TheLunarDev <153661521+Mayank2177@users.noreply.github.com>
2025-12-04 18:56:29 +0530

    Update sales.py

--- Sales_management/sales.py
+++ Sales_management/sales.py
@@ -1,1 +1,8 @@
+from google.adk.agents import LlmAgent
 
+# Sightseeing Agent: Specializes in providing sightseeing recommendations
+sightseeing_agent = LlmAgent(
+    model='gemini-2.0-flash',
+    name="SightseeingAgent",
+    description="Sightseeing information agent",
+    instruction=f"""You are a sightseeing information agent... You always return a valid JSON...""")
