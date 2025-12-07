d38e122ad4be3f072ef6bd86fc774cfd6217c1d7
TheLunarDev <153661521+Mayank2177@users.noreply.github.com>
2025-12-07 16:07:03 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -17,40 +17,24 @@ root_agent = LlmAgent(
     sub_agents=[flight_agent, hotel_agent, sightseeing_agent] # The coordinator manages these sub-agents
 )
 
-from Client_onboarding/client.py import 
-# Convert specialized agents into AgentTools
-flight_tool = agent_tool.AgentTool(agent=flight_agent)
-hotel_tool = agent_tool.AgentTool(agent=hotel_agent)
-sightseeing_tool = agent_tool.AgentTool(agent=sightseeing_agent)
 
+from Client_onboarding.client import ClientonboardingAgent
+from Project_management.project import ProjectAgent
+from Sales_management.sales import SalesAgent
+
+# Convert specialized agents into AgentTools
+Clientonboarding_tool = agent_tool.AgentTool(agent=ClientonboardingAgent)
+Project_tool = agent_tool.AgentTool(agent=ProjectAgent)
+Sales_tool = agent_tool.AgentTool(agent=SalesAgent)
 
 
 # Root agent now uses these agents as tools
 root_agent = LlmAgent(
     model='gemini-2.0-flash',
-    name="TripPlanner",
-    instruction=f"""Acts as a comprehensive trip planner...
-    Based on the user request, sequentially invoke the tools to gather all necessary trip details...""",
+    name="Workforce",
+    instruction=f"""Enhances decision-making in Consulting Firm...
+    By automating routine tasks, improving data management through sequentially invoke the tools to gather all necessary details...""",
     
     tools=[flight_tool, hotel_tool, sightseeing_tool] # The root agent can use these tools
 )
 
-
-
-# 1. Create a parallel agent for concurrent tasks
-plan_parallel = ParallelAgent(
-    name="ParallelTripPlanner",
-    sub_agents=[flight_agent, hotel_agent], # These run in parallel
-)
-
-# 2. Create a summary agent to gather results
-trip_summary = LlmAgent(
-    name="TripSummaryAgent",
-    instruction="Summarize the trip details from the flight, hotel, and sightseeing agents...",
-    output_key="trip_summary")
-
-# 3. Create a sequential agent to orchestrate the full workflow
-root_agent = SequentialAgent(
-    name="PlanTripWorkflow",
-    # Run tasks in a specific order, including the parallel step
-    sub_agents=[sightseeing_agent, plan_parallel, trip_summary])
